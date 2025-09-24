8851b29819018e442c009c45aade60d17158b6bd
JuanJoseblandon <blandonarangojuanjose@gmail.com>
2025-09-24 17:12:48 +0200

    ejercicios iniciales de java

---
+++ PROGRAMING/netbeans/trimestre1/boletin1/Ejercicios_boletin1/src/ejercicios_boletin1/Ejercicios_boletin1.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicios_boletin1;
+
+/**
+ *
+ * @author Juan Jose Blandón
+ */
+import java.util.Scanner;
+public class Ejercicios_boletin1 {
+    // Ejercicio 1
+   public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce un número entero: ");
+        int input = sc.nextInt();
+        System.out.println("El número entero  introducido es "+ input);
+                
+    }
+    
+}
---
+++ PROGRAMING/netbeans/trimestre1/boletin1/Ejercicios_boletin1/src/ejercicios_boletin1/ejercicio2.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicios_boletin1;
+/**
+ *
+ * @author Juan jose blandon
+ */
+//Pedir al usuario su edad y mostrar la que tendrá el próximo año
+public class ejercicio2 {
+    public static void main(String[] args) {
+        
+    }
+}
