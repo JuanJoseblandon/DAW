25edfad3925d154937f2f029fdb26d2dd527c05a
JuanJoseblandon <blandonarangojuanjose@gmail.com>
2025-09-30 18:34:15 +0200

    mas ejercicios de java

---
+++ PROGRAMING/netbeans/trimestre1/boletin1/Ejercicios_boletin1/src/ejercicios_boletin1/pruebas.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicios_boletin1;
+
+/**
+ *
+ * @author Alumno Tarde
+ */
+//import java.util.Scanner;
+public class pruebas {
+    //uso del  switch
+    public static void main(String[] args) {
+//        char opcion;
+//        Scanner sc =  new Scanner (System.in);
+//        do {
+//        System.out.println("********************");
+//        System.out.println("* 1 - Player One   *");
+//        System.out.println("* 2 - Player Two   *");
+//        System.out.println("* 3 - Save         *");
+//        System.out.println("* 4 - Load         *");
+//        System.out.println("* 5 - Exit         *");
+//        System.out.println("********************");
+//        System.out.println("Introduzca Opción: ");
+//        String cadena = sc.nextLine();
+//        opcion = cadena.length() > 0 ? cadena.charAt(0): '0';
+//        switch (opcion) {
+//            case '1':
+//                System.out.println("Jugador 1");
+//                break;
+//            case '2':
+//                System.out.println("Jugador 2");
+//                break;
+//            case '3':
+//                System.out.println("Save ");
+//                break;
+//            case '4':
+//                System.out.println("Load");
+//                break;
+//            case '5':
+//                System.out.println("Good bye");
+//                break;
+//
+////            default:
+////                throw new AssertionError();
+//        }
+//        }while (opcion !='5');
+    
+int a=1, b=2, c=3;
+int resultado;
+resultado = a++ == 1 && a > b++ ? a+=b:
+                 a > 0 && a== b ? a+=c:
+                          a==c ?  a++ : a--;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(resultado);
+    }
+}
