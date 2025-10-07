b63dba3c73cd09a9c7fd4704d47831a6b00cc1d3
juan jose blandon arango <blandonarangojuanjose@gmail.com>
2025-10-07 19:26:00 +0200

    ejercicios y contenidos del boletin 2

---
+++ PROGRAMING/netbeans/trimestre1/boletin2/Ejercicios_boletin2/Ejercicios_boletin2/src/ejercicios_boletin2/Ejercicio2.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicios_boletin2;
+
+/**
+ *
+ * @author WacheR
+ */
+import java.util.Scanner;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        Scanner sc =  new Scanner (System.in);           
+        System.out.println("Escribe un numero entero: ");
+        int num1 = sc.nextInt();
+        System.out.println("Escribe otro numero entero: ");
+        int num2 = sc.nextInt();
+        if (num1 > num2 ) {
+            System.out.println("El numero "+num1+" es mayor que "+num2);
+        } else {
+            if (true) {
+                
+            }
+        }
+    }
+}
